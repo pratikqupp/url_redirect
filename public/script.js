6f591ddb7654ad1b8528c4b2b3048cb88789f1dd
Pratik Pansare <113935701+pratikqupp@users.noreply.github.com>
2023-11-01 13:25:41 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,20 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
   // Extract clinicId from the URL
   const clinicId = ROOM_ID;
 
-  const apiUrl = 'https://script.google.com/macros/s/AKfycbwwdQmnQYdHo0VP2IjdiZv4ClrCeEo1TeiDx7pzAH-OyYcr0k2Q4JIrXNXCysx10jXs-g/exec'; // Replace with your API endpoint
+  const apiUrl = 'https://api.qupdev.com/provider-service/search/server/current-time'; // Replace with your API endpoint
 
-  const loader = document.querySelector('.loader');
-  const message = document.querySelector('.message');
+  // const loader = document.querySelector('.loader');
+  // const message = document.querySelector('.message');
 
   // // Show loader while making the API call
   // loader.style.display = 'block';
 
   // Make an API call to get the app store URLs
   fetch(apiUrl, {
-    method: "POST",
-    body: JSON.stringify({
-      clinicId: clinicId
-    }),
+    method: "GET",
+    // body: JSON.stringify({
+    //   clinicId: clinicId
+    // }),
     headers: {
       "Content-type": "application/json; charset=UTF-8"
     }
@@ -24,17 +24,17 @@ document.addEventListener('DOMContentLoaded', function () {
     .then((response) => response.json())
     .then((data) => {
       // Check the response for the Play Store and App Store URLs
-      const playstoreUrl = data.playstoreUrl; // Replace with the actual response property
-      const applestoreUrl = data.applestoreUrl; // Replace with the actual response property
-      if (isAndroid()) {
-        window.location.href = playstoreUrl;
-      } else if (isIOS()) {
-        window.location.href = applestoreUrl;
-      } else {
-        // If neither Android nor iOS, show an error message
-        // displayErrorMessage();
-        window.location.href = playstoreUrl;
-      }
+      // const playstoreUrl = data.playstoreUrl; // Replace with the actual response property
+      // const applestoreUrl = data.applestoreUrl; // Replace with the actual response property
+      // if (isAndroid()) {
+      //   window.location.href = playstoreUrl;
+      // } else if (isIOS()) {
+      //   window.location.href = applestoreUrl;
+      // } else {
+      //   // If neither Android nor iOS, show an error message
+      //   // displayErrorMessage();
+      //   window.location.href = playstoreUrl;
+      // }
     })
     .catch((error) => {
       console.error('API call error:', error);
