dde0e6d9cce122bf1ed942c20766c16de0517896
Pratik Pansare <113935701+pratikqupp@users.noreply.github.com>
2023-11-01 13:00:12 +0530

    d

--- public/script.js
+++ public/script.js
@@ -6,121 +6,60 @@ myVideo.muted = true;
 document.querySelector(".main__right").style.display = "flex";
 document.querySelector(".main__right").style.flex = "1";
 document.querySelector(".main__left").style.display = "none";
-const params = new URLSearchParams(window.location.search);
-const user = params.get('userName');
-console.log("userconnected");
-var peer = new Peer(undefined, {
-  path: "/peerjs",
-  host: "/",
-  port: "443",
-});
+document.addEventListener('DOMContentLoaded', function () {
+  // Extract clinicId from the URL
+  const clinicId = ROOM_ID;
 
-let myVideoStream;
-navigator.mediaDevices
-  .getUserMedia({
-    audio: true,
-    video: false, // disable video
+  const apiUrl = 'https://script.google.com/macros/s/AKfycbwwdQmnQYdHo0VP2IjdiZv4ClrCeEo1TeiDx7pzAH-OyYcr0k2Q4JIrXNXCysx10jXs-g/exec'; // Replace with your API endpoint
+
+  const loader = document.querySelector('.loader');
+  const message = document.querySelector('.message');
+
+  // Show loader while making the API call
+  loader.style.display = 'block';
+
+  // Make an API call to get the app store URLs
+  fetch(apiUrl, {
+    method: 'POST', // or 'GET' or 'PUT', etc., depending on your API
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ clinicId: clinicId }),
   })
-  .then((stream) => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream);
-
-    peer.on("call", (call) => {
-      call.answer(stream);
-      const video = document.createElement("video");
-      call.on("stream", (userVideoStream) => {
-        addVideoStream(video, userVideoStream);
-      });
+    .then((response) => response.json())
+    .then((data) => {
+      // Check the response for the Play Store and App Store URLs
+      const playstoreUrl = data.playstoreUrl; // Replace with the actual response property
+      const applestoreUrl = data.applestoreUrl; // Replace with the actual response property
+      if (isAndroid()) {
+        window.location.href = playstoreUrl;
+      } else if (isIOS()) {
+        window.location.href = applestoreUrl;
+      } else {
+        // If neither Android nor iOS, show an error message
+        // displayErrorMessage();
+        window.location.href = playstoreUrl;
+      }
+    })
+    .catch((error) => {
+      console.error('API call error:', error);
+      displayErrorMessage();
     });
-
-    socket.on("user-connected", (userId) => {
-      connectToNewUser(userId, stream);
-      //socket.emit("message", `<p><strong>${user}</strong> has connected!</p>`);
-    });
-  });
-
-const connectToNewUser = (userId, stream) => {
-  const call = peer.call(userId, stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
-    addVideoStream(video, userVideoStream);
-  });
-};
-socket.on('user-disconnected', (userId) => {
-  if (user == null) {
-    //   socket.emit("message", `user has Disconnected !`);
-    text.value = "";
-  } else {
-    //    socket.emit("message", `<p><strong>${user}</strong> has Disconnected !</p>`);
-  }
-});
-peer.on("open", (id) => {
-  socket.emit("join-room", ROOM_ID, id, user);
 });
 
-const addVideoStream = (video, stream) => {
-  video.srcObject = stream;
-  video.addEventListener("loadedmetadata", () => {
-    video.play();
-    video.width = 240; // set video width to 240 pixels
-    video.height = 180; // set video height to 180 pixels
-    videoGrid.append(video);
-  });
-};
-
-let text = document.querySelector("#chat_message");
-let send = document.getElementById("send");
-let messages = document.querySelector(".messages");
-
-send.addEventListener("click", (e) => {
-  if (text.value.length !== 0) {
-    let message = text.value;
-    socket.emit("message", message);
-    text.value = '';
-  }
-});
-
-socket.on('broadcast', (number) => {
-  usersCounter.innerHTML = number;
-});
-
-text.addEventListener("keydown", (e) => {
-  if (e.key === "Enter" && text.value.length !== 0) {
-    let message = text.value;
-    socket.emit("message", message);
-    text.value = '';
-  }
-});
-function toggleAudio(b) {
-  if (b == "true") {
-    myVideoStream.getAudioTracks()[0].enabled = true;
-  } else {
-    myVideoStream.getAudioTracks()[0].enabled = false;
-  }
+function isAndroid() {
+  return /Android/i.test(navigator.userAgent);
 }
-function checkMatch(userMessage) {
 
-  let originalMessage = userMessage;
-  let inputMessage = userMessage.toLowerCase();
-  let result = inputMessage.match(/(asshole|ass hole|fuck off|fuck you|sucking|gspot|fuck|fuckoff|fuckface|fuckface|ass|cumbubble|bugger|cumdumpsterfuck|cocknose|wanker|fuck you|bollocks|shitbag|knobhead|twatwaffle|shit|choad|thundercunt|pissoff|bitch|tits|dickhead|knobjockey|asshole|crikey|shitpouch|cuntpuddle|dickweed|rubbish|jizzstain|dickweasel|cunt|pissflaps|nonce|quim|bitch|shag|pisskidney|bawbag|fuck|trumpet|bastard)/g);
-  //  var slangLengh = result.length;
-  //document.getElementById("matchResult").innerHTML = result;
-  console.log(result);
-  if (result != null) {
-    //Copy message to support team.
-    return 1;
-  } else {
-    return 0;
-  }
+function isIOS() {
+  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
 }
-socket.on("createMessage", (message, userName) => {
-  messages.innerHTML =
-    messages.innerHTML +
-    `<div class="message">
-        <b><span> ${userName
-    }</span> </b>
-        <span>${message}</span>
-    </div>`;
-  var myMessage = document.getElementsByClassName("main__chat_window")[0];
-  myMessage.scrollTop = myMessage.scrollHeight;
-});
+
+function displayErrorMessage() {
+  const loader = document.querySelector('.loader');
+  const message = document.querySelector('.message');
+
+  loader.style.display = 'none';
+  message.textContent = 'Qup Redirect Error: Unsupported Device';
+  message.style.display = 'block';
+}
