4b082700dd863944077c775fce4c81e5e10b1656
Pratik Pansare <pratik.p@qupapp.com>
2024-01-04 19:37:06 +0530

    n

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
